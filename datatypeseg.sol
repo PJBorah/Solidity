0951d544ee92e893e53ec18860ac8a12457b3c2c
PJBorah <30236670+PJBorah@users.noreply.github.com>
2023-09-05 21:28:58 +0530

    Create datatypeseg.sol

---
+++ datatypeseg.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity  >=0.8.2 <0.9.0;
+
+contract datatypes{
+
+    int val1 = -23;// we can decalre negative or positive value like -2 or +2
+    uint val2 = 23; // we can't use negative value like age never be negative value so we can't use 
+    bool val = true; //true or false statement
+    address val ="ethereum address" //we can your account address when needed
+    bytes3 val ="abc" //we can define how many char we want to store in a value by default it take 32 char 
+    bytes4 val2 ="abc2"//now this variable only store 4 char also less gas fee
+}
